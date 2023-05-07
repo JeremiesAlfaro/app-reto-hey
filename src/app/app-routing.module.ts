2aa31b26d6e3504530cd6c5130efb3d9d4c26684
JeremiesAlfaro <a01282692@tec.mx>
2023-05-07 04:07:40 -0600

    Commit1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { WelcomeComponent } from './welcome/welcome.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { MenuComponent } from './menu/menu.component';
+
+const routes: Routes = [{
+  path: '',
+  pathMatch: 'full',
+  component: WelcomeComponent
+}, {
+  path: 'signin',
+  component: SignInComponent
+}, {
+  path: 'menu',
+  component: MenuComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ToolbarModule } from 'primeng/toolbar';
+import { ImageModule } from 'primeng/image';
+import { ButtonModule } from 'primeng/button';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { CheckboxModule } from 'primeng/checkbox';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { MenuComponent } from './menu/menu.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    SignInComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ToolbarModule,
+    ImageModule,
+    ButtonModule,
+    CardModule,
+    InputTextModule,
+    CheckboxModule,
+    RadioButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
